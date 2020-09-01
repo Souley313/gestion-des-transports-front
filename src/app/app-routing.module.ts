c848657427ec16ece192d8f65242df234e2f0547
Zbda <koulkzbda@gmail.com>
2020-09-01 11:29:37 +0200

    update menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,20 @@
+import { CollabStatistiquesComponent } from './collab-statistiques/collab-statistiques.component';
+import { CollabAnnoncesComponent } from './collab-annonces/collab-annonces.component';
+import { CollabReservationsComponent } from './collab-reservations/collab-reservations.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {TechComponent} from './tech/tech.component';
-import {StatutConnecteService} from './auth/statut-connecte.service';
-import {AuthComponent} from './auth/auth.component';
+import { TechComponent } from './tech/tech.component';
+import { StatutConnecteService } from './auth/statut-connecte.service';
+import { AuthComponent } from './auth/auth.component';
 
 
-const routes: Routes =  [
-  { path: 'tech', component: TechComponent, canActivate: [StatutConnecteService]}, // /tech accessible uniquement si connecté
-  { path: 'auth', component: AuthComponent},
-  { path: '', redirectTo: '/tech', pathMatch: 'full'}
+const routes: Routes = [
+  { path: 'tech', component: TechComponent, canActivate: [StatutConnecteService] }, // /tech accessible uniquement si connecté
+  { path: 'auth', component: AuthComponent },
+  { path: 'collaborateur/reservations', component: CollabReservationsComponent },
+  { path: 'collaborateur/annonces', component: CollabAnnoncesComponent },
+  { path: 'collaborateur/statistiques', component: CollabStatistiquesComponent },
+  { path: '', redirectTo: '/tech', pathMatch: 'full' }
 ];
 
 
