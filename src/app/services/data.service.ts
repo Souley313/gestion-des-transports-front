c0bdd784141bb889a2d6a8405540470b42889a31
Alexis PRODHOMME <alexis.prodhom@hotmail.fr>
2020-09-01 10:27:39 +0200

    Add services and backend url

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.prod';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  URL_BACKEND: string = environment.baseUrl;
+
+  constructor( private http: HttpClient) { }
+}
