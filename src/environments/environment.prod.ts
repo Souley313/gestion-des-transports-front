672806154ed74d842a56eedc62de720b943fde7e
Souleymane THIAM <souleymanethiam22@gmail.com>
2020-09-09 05:02:21 +0200

    fin: #23 #25 #26

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import { config } from './common';
 export const environment = Object.assign({
   production: true,
   //baseUrl: 'https://gestion-des-transports-back.herokuapp.com/'
-  baseUrl: 'http://localhost:8081/'
+  baseUrl: 'http://localhost:8080/'
 }, config);
